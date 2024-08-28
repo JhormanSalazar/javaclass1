0521e4240a3a489c56ec768f63c194ad1bb39eeb
JhormanSalazar <salazarjhorman181@gmail.com>
2024-08-28 10:15:28 -0500

    Add files via upload

---
+++ EMPANADASDONAMARI/src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //ENTRADAS EN JAVA
+        String nombreCliente = "Jhorman Salazar";
+        String identificacionCliente = "102353231";
+        byte edadCliente = 18;
+        String direccionCliente = "calle 65 sur # 42b-1b";
+        short saldoCanejCliente = 25000;
+        int cantidadPedidosAlMes = 10;
+        
+    }
+}
